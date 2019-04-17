c32e941021852fe61074854b848180e183509ca3
increDul0us <incredulous97@gmail.com>
2019-04-17 12:37:28 +0100

    themealdb

--- input.js
+++ input.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
 const flash = require('connect-flash');
 const session = require('express-session');
-const rp = require('request-promise');
-const cheerio = require('cheerio');
 
 const app = express();
 
@@ -35,46 +32,8 @@ app.use((req, res, next) => {
   next();
 });
 
-var mealid = [];
-var linkid = [];
-var mealarray = [];
-
-app.get('/', (req, res, ) =>{
-    res.render('input')
-})
-
-app.post('/', (req, res) =>{
-    let newmealid = req.body.mealid;
-    mealid.push(newmealid);
-    linkid.push(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${newmealid}`);
-    if (req.body.postid == 'addmore'){
-        req.flash('success_msg',newmealid + ' successfully added');
-        res.redirect(`/`);
-    }else{
-        for (var k = 0; k < linkid.length; k++){
-            const url = linkid[k];
-            console.log(url);
-            rp(url)
-              .then(function(html){
-                var mealjson = JSON.parse(html).meals[0];
-                for(var j in mealjson)
-                mealarray.push([j, mealjson [j]]);
-                for (i = 9; i < 29; i++) {
-                    console.log(mealarray[i][1])
-                    if (mealarray[i][1]===''){
-                    break
-                    }
-                }
-                req.flash('success_msg',newmealid + ' is the meal with the least ingredient');
-                res.redirect(`/`);
-              })
-              .catch(function(err){
-                console.error;  
-              });
-        }
-
-    }
-});
+// Routes
+app.use('/', require('./routes/input.js'));
 
 const PORT = process.env.PORT || 3000;
 
---
+++ routes/input.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+const router = express.Router();
+
+var mealid = [];
+var linkid = [];
+var mealarray = [];
+
+//get the meal ids
+router.get('/', (req, res, ) =>{
+    res.render('input');
+});
+
+//save the meal ids
+router.post('/', (req, res) =>{
+    let newmealid = req.body.mealid;
+    mealid.push(newmealid);
+    linkid.push(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${newmealid}`);
+    
+    //add more meal ids
+    if (req.body.postid == 'addmore'){
+        req.flash('success_msg',newmealid + ' successfully added');
+        res.redirect(`/`);
+    }
+    //submit all meal ids and get the result
+    else{
+        for (var k = 0; k < linkid.length; k++){
+            const url = linkid[k];
+            console.log(url);
+            
+            //read themealdb api for the inputted mealids
+            rp(url)
+              .then(function(html){
+                var mealjson = JSON.parse(html).meals[0];
+                for(var j in mealjson){
+                    mealarray.push([j, mealjson [j]]);
+                }
+                
+                //read the ingredients of the inputted mealids
+                for (i = 9; i < 29; i++) {
+                    console.log(mealarray[i][1])
+                    if (mealarray[i][1]===''){
+                    break
+                    }
+                }
+                req.flash('success_msg',newmealid + ' is the meal with the least ingredient');
+                res.redirect(`/`);
+              })
+              .catch(function(err){
+                console.error;  
+              });
+        }
+
+    }
+});
+
+module.exports = router;
